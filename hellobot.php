5a079765e65dc3dcecd46eec8fd0479d9c0f0bdc
adechs80 <andra.hs@gmail.com>
2016-09-01 21:08:03 +0700

    Update hellobot.php

--- hellobot.php
+++ hellobot.php
@@ -139,7 +139,7 @@ function processMessage($message) {
 }
 
 
-define('WEBHOOK_URL', 'https://my-site.example.com/secret-path-for-webhooks/');
+define('WEBHOOK_URL', 'https://github.com/adechs80/komo/blob/master/hellobot.php/');
 
 if (php_sapi_name() == 'cli') {
 	// if run from console, set or delete webhook
